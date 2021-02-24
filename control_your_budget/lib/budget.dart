35670259f96b87b506a15027e113e5a71de703e2
tarvonurmetu <tanurm@ttu.ee>
2021-02-24 13:04:46 +0200

    'Budget.dart fail lisatud'

--- control_your_budget/lib/budget.dart
+++ control_your_budget/lib/budget.dart
@@ -1,7 +1,11 @@
 class Budget {
 
+  String username;
+  String budgetName;
   int fullBudget;
   int foodBudget;
+  int transportBudget;
+  int accomodationBudget;
 
   Budget({int fB, int fB2}){
     fullBudget=fB;
