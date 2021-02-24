6317d7caa11e24d0b55d9254caeb08110abf87c0
tarvonurmetu <tanurm@ttu.ee>
2021-02-25 00:18:08 +0200

    'Lisasin BudgetName ja FullBudget inputid'

--- control_your_budget/lib/constants.dart
+++ control_your_budget/lib/constants.dart
@@ -2,7 +2,14 @@ import 'package:flutter/material.dart';
 
 const kLabelTextStyle = TextStyle(
   fontSize: 15.0,
+  color: Color(0xFF8D8E98),
 );
+
+const kLargeButtonTextStyle = TextStyle(
+  fontSize: 20.0,
+  fontWeight: FontWeight.bold,
+);
+
 
 const kBottomContainerHeight = 80.0;
 const kInactiveCardColour = Color(0xFF1D1E33);
--- control_your_budget/lib/screens/newBudget_page.dart
+++ control_your_budget/lib/screens/newBudget_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:control_your_budget/constants.dart';
+import 'package:control_your_budget/components/bottom_create_button.dart';
+import 'package:flutter/services.dart';
 
 class NewBudget extends StatefulWidget {
   @override
@@ -17,17 +19,58 @@ class _NewBudgetState extends State<NewBudget> {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Expanded(
+            //Budget name form
             flex: 1,
             child: Container(
               margin: EdgeInsets.all(15.0),
+              padding: EdgeInsets.all(5.0),
               decoration: BoxDecoration(
                 color: kInactiveCardColour,
                 borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: TextFormField(
+                textAlign: TextAlign.center,
+                decoration: const InputDecoration(
+                  hintText: 'Enter name for Budget',
+                  labelText: 'Budget Name:',
+                ),
+                maxLength: 15,
+                onFieldSubmitted: (text) {
+                  print(text);
+                },
               ),
             ),
           ),
           Expanded(
-            flex: 3,
+            flex: 1,
+            child: Container(
+              margin: EdgeInsets.all(15.0),
+              padding: EdgeInsets.all(5.0),
+              decoration: BoxDecoration(
+                color: kInactiveCardColour,
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: TextFormField( // TODO: Leia viis kuidas, sisesta ainult rahaformaadis
+                keyboardType: TextInputType.numberWithOptions(decimal: true),
+                inputFormatters: [
+                  FilteringTextInputFormatter.allow(
+                    RegExp(r'[0-9]'),
+                  ),
+                ],
+                textAlign: TextAlign.center,
+                decoration: const InputDecoration(
+                  hintText: 'Enter Budget amount',
+                  labelText: 'Budget:',
+                ),
+                maxLength: 15,
+                onFieldSubmitted: (text) {
+                  print(text);
+                },
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 5,
             child: Container(
               margin: EdgeInsets.all(15.0),
               decoration: BoxDecoration(
@@ -36,16 +79,12 @@ class _NewBudgetState extends State<NewBudget> {
               ),
             ),
           ),
-          Container(
-            child: Center(
-              child: Text('Create Budget'),
-            ),
-            color: kBottomContainerColour,
-            margin: EdgeInsets.only(top: 10.0),
-            padding: EdgeInsets.only(bottom: 20.0),
-            width: double.infinity,
-            height: kBottomContainerHeight,
-          ),
+          BottomButton(
+            onTap: () {
+              print('Bottom button pushed.');
+            },
+            buttonTitle: 'CREATE BUDGET',
+          )
         ],
       ),
     );
